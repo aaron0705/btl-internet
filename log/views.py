a8fbd955d72a4e76913688dd4a7f7ced71c3b9d0
aaron0705 <167523231+aaron0705@users.noreply.github.com>
2025-09-30 22:20:30 +0700

    Update views.py

--- log/views.py
+++ log/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 import pymysql
 from datetime import datetime, timedelta
 import re
